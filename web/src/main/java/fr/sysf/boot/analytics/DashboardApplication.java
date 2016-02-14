307cb8d0dc86198a40692adbe9a7f758cc7d0026
Florent Peyron <florentpeyron@gmail.com>
2016-02-14 14:18:08 +0100

    add module web

---
+++ web/src/main/java/fr/sysf/boot/analytics/DashboardApplication.java
@@ -0,0 +1,15 @@
+package fr.sysf.boot.analytics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by florent on 14/02/2016.
+ */
+@SpringBootApplication
+public class DashboardApplication {
+
+    public void main(String[] args) {
+        SpringApplication.run(DashboardApplication.class, args);
+    }
+}
